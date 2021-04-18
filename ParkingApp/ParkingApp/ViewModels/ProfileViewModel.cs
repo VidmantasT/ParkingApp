083bd1c23da1cbfbb6ee173eed91a536f2f36b8c
VidmantasT <50624497+VidmantasT@users.noreply.github.com>
2021-04-18 17:18:37 +0300

    Meniu juosta

---
+++ ParkingApp/ParkingApp/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace ParkingApp.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public Command GoToPage { get; }
+        public ProfileViewModel()
+        {
+            GoToPage = new Command(async() => await SendEmail());
+        }
+
+        public async Task SendEmail()
+        {
+            try
+            {
+                await Email.ComposeAsync("ParkingApp", "", "vitelksnys@gmail.com");
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
--- ParkingApp/ParkingApp/Views/Profile.xaml.cs
+++ ParkingApp/ParkingApp/Views/Profile.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ParkingApp.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,8 @@ namespace ParkingApp.Views
         public Profile()
         {
             InitializeComponent();
+
+            BindingContext = new ProfileViewModel();
         }
     }
 }
