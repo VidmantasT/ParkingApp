ee200c3f192ef75ded8c51838516261ec7029f87
VidmantasT <50624497+VidmantasT@users.noreply.github.com>
2021-04-20 02:14:54 +0300

    Add car functionality

--- ParkingApp/ParkingApp/ViewModels/ProfileViewModel.cs
+++ ParkingApp/ParkingApp/ViewModels/ProfileViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Essentials;
@@ -7,13 +9,89 @@ using Xamarin.Forms;
 
 namespace ParkingApp.ViewModels
 {
-    public class ProfileViewModel
+    public class ProfileViewModel : INotifyPropertyChanged
     {
-        public bool isEnabled { get; set; }
+        public bool isToggled = false;
+        public bool IsToggled
+        {
+            get { return isToggled; }
+            set
+            {
+                isToggled = value;
+                IsEnabled = value;
+            }
+        }
+
+        public string brand = "";
+        public string Brand
+        {
+            get { return brand; }
+            set 
+            { 
+                brand = value;
+            }
+        }
+
+        public string licencePlate = "";
+        public string LicencePlate
+        {
+            get { return licencePlate; }
+            set 
+            { 
+                licencePlate = value;
+            }
+        }
+
+        public bool isEnabled = false;
+        public bool IsEnabled
+        {
+            get { return isEnabled; }
+            set
+            {
+                isEnabled = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public string DisplayBrand
+        {
+            get { return brand; }
+        }
+
+        public string DisplayLicencePlate
+        {
+            get { return licencePlate; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
         public Command sendEmail { get; }
+        public Command saveCar { get; }
+
         public ProfileViewModel()
         {
             sendEmail = new Command(async() => await SendEmail());
+            saveCar = new Command(async () => await SaveCar(this.brand, this.licencePlate));
+        }
+
+        public async Task SaveCar(string brand, string licencePlate)
+        {
+            if (this.brand == "" || this.licencePlate == "")
+                await Application.Current.MainPage.DisplayAlert("Alert", "Enter empty fields!", "OK");
+            else
+            {
+                Brand = brand;
+                LicencePlate = licencePlate;
+
+                OnPropertyChanged(nameof(DisplayBrand));
+                OnPropertyChanged(nameof(DisplayLicencePlate));
+            }
         }
 
         public async Task SendEmail()
@@ -24,6 +102,7 @@ namespace ParkingApp.ViewModels
             }
             catch (Exception ex)
             {
+                await Application.Current.MainPage.DisplayAlert("Alert", $"{ex}", "OK");
             }
         }
     }
